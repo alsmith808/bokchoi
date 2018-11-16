cb3c304fefc3766c15f07d180b2d4a5e3d0f55d8
Al Smit <solanus@gmail.com>
2018-11-16 13:07:27 +0000

    user update profile functionality added

--- bokchoi/forms.py
+++ bokchoi/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from bokchoi.models import User
@@ -7,7 +9,7 @@ from bokchoi.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=5, max=12)])
+                           validators=[DataRequired(), Length(min=5, max=30)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -16,14 +18,14 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+      user = User.query.filter_by(username=username.data).first()
+      if user:
+        raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+      user = User.query.filter_by(email=email.data).first()
+      if user:
+        raise ValidationError('That email is taken. Please choose a different one.')
 
 
 
@@ -34,3 +36,26 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=5, max=30)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+          user = User.query.filter_by(username=username.data).first()
+          if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+          user = User.query.filter_by(email=email.data).first()
+          if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
